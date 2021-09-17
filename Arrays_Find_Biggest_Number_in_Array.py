8ab8abad459af401bc4ca3d60ca1188ae5ce73b9
RAHIL SHARMA <67217341+rahil1303@users.noreply.github.com>
2021-09-17 15:01:54 +0530

    Create Arrays_Find_Biggest_Number_in_Array.py

---
+++ Arrays_Find_Biggest_Number_in_Array.py
@@ -0,0 +1,9 @@
+def findbiggestNumber(sampleArray):
+  biggestNumber = sampleArray[0]
+  for index in range(1,len(sampleArray)):
+    if sampleArray[index] > biggestNumber:
+      biggestNumber = sampleArray[index]
+   
+  print(biggestNumber)
+  
+ print(findbiggestNumber([9,5,4,6,2,1])
